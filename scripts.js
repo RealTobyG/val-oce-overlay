bad1cee5e922e4e98a81f08f0fc38bf73c1eca14
caleb-cb <calebcolebaker@gmail.com>
2024-07-16 23:10:54 +1200

    Console functionality update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 const TeamAName = document.querySelector("#team-a-name")
 const TeamBName = document.querySelector("#team-b-name")
+const TeamATri = document.querySelector("#team-a-tri")
+const TeamBTri = document.querySelector("#team-b-tri")
 
 const TeamANameIGO = document.querySelector("#team-a-name-igo")
 const TeamBNameIGO = document.querySelector("#team-b-name-igo")
@@ -28,6 +30,17 @@ const mapdata = [
     {mapname: "Sunset", mapimg: "n/a", mappool: true},
 ]
 
+const mapvetobo3 = [
+    {mapname: "map", team: "team", def: "na"},
+    {mapname: "map", team: "team", def: "na"},
+    {mapname: "map", team: "team", def: "team"},
+    {mapname: "map", team: "team", def: "team"},
+    {mapname: "map", team: "team", def: "na"},
+    {mapname: "map", team: "team", def: "na"},
+    {mapname: "map", team: "decider", def: "team"},
+]
+
+let mapveto = []
 
 // Overlay Template Selection
 function setOverlay() {
@@ -46,13 +59,31 @@ OverlaySelection.addEventListener("change", setOverlay)
 
 
 // Team A/B Name Changing
-function setIGOnames() {
+function setTeamNames() {
+    const TeamATriAll = document.getElementsByClassName('apply-team-a-tri');
+    const TeamBTriAll = document.getElementsByClassName('apply-team-b-tri');
+    const TeamAFullAll = document.getElementsByClassName('apply-team-a-full');
+    const TeamBFullAll = document.getElementsByClassName('apply-team-b-full');
+    for (const instance of TeamATriAll) {
+        instance.textContent = `${TeamATri.value}`
+    }
+    for (const instance of TeamBTriAll) {
+        instance.textContent = `${TeamBTri.value}`
+    }
+    for (const instance of TeamAFullAll) {
+        instance.textContent = `(${TeamATri.value}) ${TeamAName.value}`
+    }
+    for (const instance of TeamBFullAll) {
+        instance.textContent = `(${TeamBTri.value}) ${TeamBName.value}`
+    }
     TeamANameIGO.textContent = `${TeamAName.value}`
     TeamBNameIGO.textContent = `${TeamBName.value}`
 }
 
-TeamAName.addEventListener("keyup", setIGOnames);
-TeamBName.addEventListener("keyup", setIGOnames);
+TeamAName.addEventListener("keyup", setTeamNames);
+TeamBName.addEventListener("keyup", setTeamNames);
+TeamATri.addEventListener("keyup", setTeamNames);
+TeamBTri.addEventListener("keyup", setTeamNames);
 
 
 //Swap Teams Button
@@ -119,3 +150,44 @@ function setMapPool() {
 }
 
 MapPoolSelection.addEventListener("change", setMapPool)
+
+
+// Map Veto Logic
+function setMapVeto() {
+    const mapSelections = document.getElementsByClassName("map-selection-maps")
+    const teamSelections = document.getElementsByClassName("map-selection-teams")
+    const sideSelections = document.getElementsByClassName("side-selection-teams")
+    let index = 0
+    mapveto = []
+    if (SeriesLengthSelection.value === "BO3") {
+        for (const map of mapSelections) {
+            if (map.classList.contains("bo3")) {
+                let selectedMap = {mapname: `${map.value}`}
+                mapveto.push(selectedMap);
+            }
+        }
+    }
+}
+
+function mapVetoActivate() {
+    const mapSelections = document.getElementsByClassName("map-selection-maps")
+    const teamSelections = document.getElementsByClassName("map-selection-teams")
+    const sideSelections = document.getElementsByClassName("side-selection-teams")
+    for (const element of mapSelections || teamSelections || sideSelections) {
+        element.addEventListener("change", setMapVeto) 
+    }
+}
+
+
+//Score Keeping Logic
+function scoreUpdate() {
+    
+}
+
+
+//Run On Page Load
+function pageLoad() {
+    setMapPool()
+    mapVetoActivate()
+    setMapVeto()
+}
