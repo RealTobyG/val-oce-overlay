abefdbd851a1b4b879445fb2d4f9601b6047958c
caleb-cb <calebcolebaker@gmail.com>
2024-07-18 00:57:13 +1200

    Basic map veto functionality added

--- scripts.js
+++ scripts.js
@@ -3,11 +3,6 @@ const TeamBName = document.querySelector("#team-b-name")
 const TeamATri = document.querySelector("#team-a-tri")
 const TeamBTri = document.querySelector("#team-b-tri")
 
-const TeamANameIGO = document.querySelector("#team-a-name-igo")
-const TeamBNameIGO = document.querySelector("#team-b-name-igo")
-const TeamALogoIGO = document.querySelector("#team-a-logo-igo")
-const TeamBLogoIGO = document.querySelector("#team-b-logo-igo")
-
 const SwapTeams = document.querySelector("#swap-teams")
 
 const OverlaySelection = document.querySelector("#overlay-selection")
@@ -40,18 +35,25 @@ const mapvetobo3 = [
     {mapname: "map", team: "decider", def: "team"},
 ]
 
-let mapveto = []
+let mappicks = []
+let mapbans = []
 
 // Overlay Template Selection
 function setOverlay() {
     const SelectedOption = document.getElementById('overlay-selection').value
-    const IGO = document.querySelector("#in-game-overlay")
-    if (SelectedOption === "GC2024") {
-        IGO.setAttribute("class", "gc2024-igo")
-    } else if (SelectedOption === "VCT2024") {
-        IGO.setAttribute("class", "vct2024-igo")
+    const overlayElements = document.getElementsByClassName('overlay-element')
+    if (SelectedOption === "gc") {
+        for (const element of overlayElements) {
+            element.src = element.src.replace(/(VCL|LPL)/g, "GC")
+        }
+    } else if (SelectedOption === "vcl") {
+        for (const element of overlayElements) {
+            element.src = element.src.replace(/(GC|LPL)/g, "VCL")
+        }
     } else {
-        IGO.setAttribute("class", "lpl-igo")
+        for (const element of overlayElements) {
+            element.src = element.src.replace(/(VCL|GC)/g, "LPL")
+        }
     }
 }
 
@@ -60,10 +62,18 @@ OverlaySelection.addEventListener("change", setOverlay)
 
 // Team A/B Name Changing
 function setTeamNames() {
+    const TeamANameAll = document.getElementsByClassName('apply-team-a-name');
+    const TeamBNameAll = document.getElementsByClassName('apply-team-b-name');
     const TeamATriAll = document.getElementsByClassName('apply-team-a-tri');
     const TeamBTriAll = document.getElementsByClassName('apply-team-b-tri');
     const TeamAFullAll = document.getElementsByClassName('apply-team-a-full');
     const TeamBFullAll = document.getElementsByClassName('apply-team-b-full');
+    for (const instance of TeamANameAll) {
+        instance.textContent = `${TeamAName.value}`
+    }
+    for (const instance of TeamBNameAll) {
+        instance.textContent = `${TeamBName.value}`
+    }
     for (const instance of TeamATriAll) {
         instance.textContent = `${TeamATri.value}`
     }
@@ -76,8 +86,6 @@ function setTeamNames() {
     for (const instance of TeamBFullAll) {
         instance.textContent = `(${TeamBTri.value}) ${TeamBName.value}`
     }
-    TeamANameIGO.textContent = `${TeamAName.value}`
-    TeamBNameIGO.textContent = `${TeamBName.value}`
 }
 
 TeamAName.addEventListener("keyup", setTeamNames);
@@ -88,6 +96,10 @@ TeamBTri.addEventListener("keyup", setTeamNames);
 
 //Swap Teams Button
 function swapTeamsIGO() {
+    const TeamANameIGO = document.querySelector("#team-a-name-igo")
+    const TeamBNameIGO = document.querySelector("#team-b-name-igo")
+    const TeamALogoIGO = document.querySelector("#team-a-logo-igo")
+    const TeamBLogoIGO = document.querySelector("#team-b-logo-igo")
     if (TeamANameIGO.classList.contains("team-name-left-igo")) {
         TeamANameIGO.setAttribute("class", "team-name-right-igo")
         TeamBNameIGO.setAttribute("class", "team-name-left-igo")
@@ -122,6 +134,7 @@ function setSeriesLength() {
         bo3Console.setAttribute("class", "inactive-series-console")
         bo5Console.setAttribute("class", "inactive-series-console")
     }
+    setMapVeto()
 }
 
 SeriesLengthSelection.addEventListener("change", setSeriesLength)
@@ -140,11 +153,16 @@ function setMapSelection(selectElement) {
             selectElement.appendChild(newOption)
         }
     }
+    setMapVeto()
 }
 
 function setMapPool() {
-    const mapSelectMenus = document.getElementsByClassName("map-selection-maps")
-    for (const menu of mapSelectMenus) {
+    const mapBansAll = document.getElementsByClassName("map-ban-selection")
+    const mapPicksAll = document.getElementsByClassName("map-pick-selection")
+    for (const menu of mapBansAll) {
+        setMapSelection(menu)
+    }
+    for (const menu of mapPicksAll) {
         setMapSelection(menu)
     }
 }
@@ -154,26 +172,48 @@ MapPoolSelection.addEventListener("change", setMapPool)
 
 // Map Veto Logic
 function setMapVeto() {
-    const mapSelections = document.getElementsByClassName("map-selection-maps")
+    const mapBansAll = document.getElementsByClassName("map-ban-selection")
+    const mapPicksAll = document.getElementsByClassName("map-pick-selection")
     const teamSelections = document.getElementsByClassName("map-selection-teams")
     const sideSelections = document.getElementsByClassName("side-selection-teams")
-    let index = 0
-    mapveto = []
+    mappicks = []
+    mapbans = []
     if (SeriesLengthSelection.value === "BO3") {
-        for (const map of mapSelections) {
+        for (const map of mapBansAll) {
             if (map.classList.contains("bo3")) {
                 let selectedMap = {mapname: `${map.value}`}
-                mapveto.push(selectedMap);
+                mapbans.push(selectedMap);
             }
         }
+
+        for (const map of mapPicksAll) {
+            if (map.classList.contains("bo3")) {
+                let selectedMap = {mapname: `${map.value}`}
+                mappicks.push(selectedMap);
+            }
+        }
+
+        const pickImgs = document.getElementsByClassName('bo3-pick-img')
+        Array.from(pickImgs).forEach((img, i) => {
+            img.src = `assets/Maps/${mappicks[i].mapname}_320x640.png`
+        })
+
+        const banImgs = document.getElementsByClassName('bo3-ban-img')
+        Array.from(banImgs).forEach((img, i) => {
+            img.src = `assets/Maps/${mapbans[i].mapname}_320x320.png`
+        })
     }
 }
 
 function mapVetoActivate() {
-    const mapSelections = document.getElementsByClassName("map-selection-maps")
+    const mapBansAll = document.getElementsByClassName("map-ban-selection")
+    const mapPicksAll = document.getElementsByClassName("map-pick-selection")
     const teamSelections = document.getElementsByClassName("map-selection-teams")
     const sideSelections = document.getElementsByClassName("side-selection-teams")
-    for (const element of mapSelections || teamSelections || sideSelections) {
+    for (const element of mapBansAll) {
+        element.addEventListener("change", setMapVeto) 
+    }
+    for (const element of mapPicksAll) {
         element.addEventListener("change", setMapVeto) 
     }
 }
