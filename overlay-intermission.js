64d81278a8350489bf21792f29725bb8944bca9e
caleb-cb <calebcolebaker@gmail.com>
2024-09-23 00:00:57 +1200

    Intermission overlay fully functional with backend

--- overlay-intermission.js
+++ overlay-intermission.js
@@ -126,20 +126,20 @@ function setOverlay() {
     if (overlaySetup.seriesLengthSelection === 1) {        
         if (overlaySetup.teamASeriesScore !== 2 && overlaySetup.teamBSeriesScore !== 2) {
             for (const element of currentMap) {
-                element.textContent = `Map ${overlaySetup.mapNumber+1} - ${mapPicks[overlaySetup.mapNumber]}`
+                element.textContent = `Map ${overlaySetup.mapNumber+1} - ${overlaySetup.mapPicks[overlaySetup.mapNumber]}`
             }
         } else if (overlaySetup.teamASeriesScore>overlaySetup.teamBSeriesScore) {
             for (const element of currentMap) {
-                element.textContent = `${TeamATri.value} ${teamASeriesScore} - ${teamBSeriesScore} ${TeamBTri.value}`
+                element.textContent = `${overlaySetup.teamATri} ${overlaySetup.teamASeriesScore} - ${overlaySetup.teamBSeriesScore} ${overlaySetup.teamBTri}`
             }
         } else {
             for (const element of currentMap) {
-                element.textContent = `${TeamBTri.value} ${teamBSeriesScore} - ${teamASeriesScore} ${TeamATri.value}`
+                element.textContent = `${overlaySetup.teamBTri} ${overlaySetup.teamBSeriesScore} - ${overlaySetup.teamASeriesScore} ${overlaySetup.teamBTri}`
             }
         }
-        if (mapNumber<3) {
+        if (overlaySetup.mapNumber<3) {
             Array.from(bo3IntermissionBackgroundMap).forEach((element, i) => {
-                if (i === mapNumber) {
+                if (i === overlaySetup.mapNumber) {
                     element.style.opacity = 100
                 } else {
                     element.style.opacity = 0
@@ -148,16 +148,36 @@ function setOverlay() {
         }    
     }
     // Shows/Hides map results for finished maps on map veto and intermission overlay
-    const mapResultOverlays = document.getElementsByClassName('map-result-overlay')
-    Array.from(mapResultOverlays).forEach((element, i) => {
-        if (Number(i)+1<=mapNumber) {
+    Array.from(scheduleResultOverlays).forEach((element, i) => {
+        if (Number(i)+1<=overlaySetup.mapNumber) {
             element.style.display = 'flex'
-            scheduleResultOverlays[i].style.display = 'flex'
         }
         else {
             element.style.display = 'none'
-            scheduleResultOverlays[i].style.display = 'none'
         }
+    })
+    // Sets logos on intermission screen 
+    const intermissionDefTeams = document.getElementsByClassName('bo3-def-logo')
+    const intermissionAttackTeams = document.getElementsByClassName('bo3-attack-logo')
+    overlaySetup.mapPicksSides.forEach((team, i) => {
+        if (team === 'team-a') {
+            intermissionDefTeams[i].className = intermissionDefTeams[i].className.replace(/(team-a|team-b)/g, `team-a`)
+            intermissionAttackTeams[i].className = intermissionAttackTeams[i].className.replace(/(team-a|team-b)/g, `team-b`)
+        } else {
+            intermissionDefTeams[i].className = intermissionDefTeams[i].className.replace(/(team-a|team-b)/g, `team-b`)
+            intermissionAttackTeams[i].className = intermissionAttackTeams[i].className.replace(/(team-a|team-b)/g, `team-a`)
+        }
+    })
+    // Sets map pick team names on intermission overlay
+    const pickNamesIntermission = document.getElementsByClassName('bo3-pick-team-intermission')
+    overlaySetup.mapPicksTeams.forEach((pick, i) => {
+        pickNamesIntermission[i].className = pickNamesIntermission[i].className.replace(/(team-a|team-b)/g, `${pick}`)
+    })
+    // Sets map names and images for each map pick on map veto overlay and intermission overlay
+    const pickImgsIntermission = document.getElementsByClassName('bo3-pick-img-intermission')
+    overlaySetup.mapPicks.forEach((pick, i) => {
+        pickImgsIntermission[i].src = `assets/Maps/${pick}_524x214.png`
+        bo3IntermissionBackgroundMap[i].src = `https://github.com/caleb-cb/community-caster-overlay-videos/blob/main/${pick}_Cinematic.webm?raw=true`
     })
 
     // Set Names
@@ -204,5 +224,18 @@ function setOverlay() {
     }
     for (const element of eventLogoAll) {
         element.src = overlaySetup.eventLogo
+    }
+}
+
+function playAllVideo() {
+    const AllVideos = document.getElementsByClassName('video')
+    for (const element of AllVideos) {
+        element.play()
+    }
+}
+function pauseAllVideo() {
+    const AllVideos = document.getElementsByClassName('video')
+    for (const element of AllVideos) {
+        element.pause()
     }
 }
